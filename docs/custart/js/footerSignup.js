601852c8972a3993be72f48869b5deb96011574a
ovaramicy99 <141614890+ovaramicy99@users.noreply.github.com>
2025-09-18 14:46:25 +0700

    Add files via upload

---
+++ docs/custart/js/footerSignup.js
@@ -0,0 +1,35 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+(function(t){var e={};function r(n){if(e[n])return e[n].exports;var i=e[n]={exports:{},id:n,loaded:false};t[n].call(i.exports,i,i.exports,r);i.loaded=true;return i.exports}r.m=t;r.c=e;r.p="https://web.archive.org/web/20250616180628/https://cdn2.editmysite.com/js/";r.p="https://"+window.ASSETS_BASE+"/js/"||r.p;return r(0)})({0:function(t,e,r){t.exports=r(610)},610:function(t,e){(function(t,e){var r={height:62,mobileHeight:124,getHeight:function(){if(u()){return r.mobileHeight}return r.height}};function n(e,n){var u=t("#weebly-footer-signup-container-v3");if(!u.length){return}i(e,n);r.element=u;r.iframe=t("#weebly-footer-signup-iframe");if(!o()){r.element.remove();return}a();s();l();t(window).on({resize:p(l,500),scroll:p(l,500)})}function i(e,r){var n='<link href="//'+e+"/css/free-footer-v3.css?buildtime="+r+'" rel="stylesheet">';t(n).appendTo("head")}function o(){var e=t("body");var r=!!document.getElementById("kb-container");var n=e.hasClass("splash-page");return!(r||n)}function a(){var e=t("body");e.css({minHeight:"100%",position:"relative"});document.body.style.setProperty("height","auto","important");e.append(r.element)}function s(){var e=t("#weebly-footer-signup-container-v3");var r=500;t(".signup-container-header").hover(p(function(){if(u()){return}var t="288px";e.animate({height:t},r)},r*2,true),p(function(){if(u()){return}var t="62px";e.animate({height:t},r)},r,false))}function p(t,e,r){var n;return function(){var i=this;var o=arguments;var a=function(){n=null;if(!r)t.apply(i,o)};var s=r&&!n;clearTimeout(n);n=setTimeout(a,e);if(s)t.apply(i,o)}}function l(){var e=r.element[0];f(e);y();var n=t("#wsite-mini-cart");if(n.length){if(window.innerHeight-n[0].getBoundingClientRect().bottom<=0){e.style.setProperty("display","none","important")}}var i=window.location.pathname.indexOf("/store/checkout/")!==-1;if(i&&c()){e.style.setProperty("display","none","important")}window.container=r}function u(){var t=window.matchMedia("(max-width: 480px)");return t?t.matches:true}function c(){var t=window.matchMedia("(max-width: 800px)");return t?t.matches:true}function f(e){var n=r.element.find("*");n.push(e);n.each(function(){var e=t(this);var r=e[0];var n="block";if(e.hasClass("weebly-icon")){n="inline-block"}r.style.setProperty("visibility","visible","important");r.style.setProperty("display",n,"important")});e.style.setProperty("height",r.getHeight()+"px","important");e.style.setProperty("position","absolute","important");e.style.setProperty("bottom","0","important");e.style.setProperty("left","0","important");e.style.setProperty("right","0","important");e.style.setProperty("position","fixed","important");e.style.setProperty("width","290px","important");e.style.setProperty("left","10px","important");e.style.setProperty("z-index","20","important");if(u()){e.style.setProperty("width","100%","important");e.style.setProperty("height","45px","important");e.style.setProperty("left","0px","important");e.style.setProperty("z-index","5","important");t("img.footer-ab-published-toast-image").css("display","none","important");t("span.footer-ab-published-toast-text").css("display","none","important")}var i=t("body");var o=i.css("paddingBottom");var a=r.element.outerHeight(true)+"px";if(o!==a){document.body.style.setProperty("padding-bottom",a,"important")}}function y(){var e=t(".header");var r=t("#weebly-footer-signup-container-v3");var n=0;var i=setInterval(function(){n++;if(e.hasClass("stuck")){e.css("padding","10px 40px 60px");clearInterval(i)}else{if(u()){r.css("left","0px")}else{r.css("left","10px")}}if(n===5){clearInterval(i)}},200)}e.footer={setupContainer:n}})(window.jQuery,window.Weebly)}});
+
+}
+/*
+     FILE ARCHIVED ON 18:06:28 Jun 16, 2025 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 05:58:14 Sep 18, 2025.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.905
+  exclusion.robots: 0.048
+  exclusion.robots.policy: 0.03
+  esindex: 0.015
+  cdx.remote: 19.813
+  LoadShardBlock: 58.724 (3)
+  PetaboxLoader3.datanode: 71.174 (4)
+  load_resource: 73.289
+  PetaboxLoader3.resolve: 39.934
+*/
